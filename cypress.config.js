3d7830a4af65be56393ad7db94fcfbbe94d9da61
BrindhaRaviChitra <brindha.rc@tranetechnologies.com>
2025-11-27 12:02:49 +0530

    dataload

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: "bcitmf",
+    projectId: "ss8p1a",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
